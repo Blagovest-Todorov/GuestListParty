61b1a5740bb7a29c0e6d5a571ce2585897d039a8
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-13 21:11:55 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbCommands = int.Parse(Console.ReadLine());
+            List<string> people = new List<string>(numbCommands);
+
+            for (int i = 0; i < numbCommands; i++)
+            {
+                string[] message = Console.ReadLine().Split(' ');
+                string namePerson = message[0]; //on the first place always is the Name
+
+                if (message.Length == 3)
+                {
+                    if (!(people.Contains(namePerson)))
+                    {
+                        people.Add(namePerson);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{namePerson} is already in the list!");
+                    }
+
+                }
+                else // if the message != 3 -> all the rest cases;
+                {
+                    if (people.Contains(namePerson))
+                    {
+                        people.Remove(namePerson);
+                    }
+                    else // if (!people.Contains(namePerson))
+                    {
+                        Console.WriteLine($"{namePerson} is not in the list!");
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join('\n', people));
+        }
+    }
+}
